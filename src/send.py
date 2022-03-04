34036d397f6de64a01809f1c5e56caba467a9087
damianobellucci <belluccidamiano@gmail.com>
2022-03-04 20:37:14 +0100

    bug fixed

--- src/send.py
+++ src/send.py
@@ -1,14 +1,25 @@
 from scrape import scrape
 import requests
 import time
+import json
+
 
 def send_messages():
-    url = "https://api.telegram.org/bot5168034242:AAFd-MnqUnY1lv1EDVqd63YxLeYvcUS3lHY/sendMessage?chat_id=@newschannel1011&text="
+
+    file_config= open('../data/config.json')
+    config = json.load(file_config)
+    
+    token = config['token-telegram-bot']
+    channel_id = config['channel_id-telegram']
+        
+
+    url = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id=@"+channel_id+"&text="
 
     messages = scrape()
 
 
     for el in messages:
         message = el['title']+'\n'+el['body']
+        #send in timestamp order
         time.sleep(0.5)
         requests.get(url+message)
