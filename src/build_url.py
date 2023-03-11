cbc0782b61ef63a96b1f0a4935b76fee19e8cce4
Damiano <belluccidamiano@gmail.com>
2023-03-11 13:27:48 +0100

    update

--- src/build_url.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import json
-
-#this function create the correct url for scraping, with updated date
-def build_url():
-    file_config= open('./data/config.json')
-    config = json.load(file_config)
-    url = config["url"]
-    return url
--- src/minun.py
+++ src/minun.py
@@ -1,9 +1,21 @@
+import time
+import json
 from bs4 import BeautifulSoup
 import requests
 import unidecode
 from datetime import datetime, timedelta
-from build_url import build_url
 
+debug = True
+
+current_date = datetime.today().strftime('%Y/%m/%d')
+print(current_date)
+
+# this function create the correct url for scraping, with updated date
+def build_url():
+    file_config = open("./data/config.json")
+    config = json.load(file_config)
+    url = config["url"]
+    return url
 
 def format_time(string_time):
     info_time = string_time
@@ -12,37 +24,23 @@ def format_time(string_time):
     final_time = date + " - " + info_time[len(info_time) - 8 :]
     return final_time
 
-
 def scrape():
-
     url = build_url()
-
     res = requests.get(url)
-
     soup = BeautifulSoup(res.text, "html.parser")
-
     # filter articles blocks
     blocks = soup.find_all("section", {"class": "lb-post lb-type-HTML lb-tagName-"})
-
     list = []
-
     for block in blocks:
         soup = BeautifulSoup(str(block), "html.parser")
-
         id = soup.section["id"]
-
         article = soup.find("div", {"class": "lb-body"})
-
         # timestamp of article
         time = soup.find("time")["datetime"][:-5]
-
         time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
         time = time + timedelta(hours=1)
-
         time_string = format_time(time.ctime())
-
         timestamp = datetime.timestamp(time)
-
         time = {
             "month": time.month,
             "day": time.day,
@@ -50,13 +48,11 @@ def scrape():
             "minute": time.minute,
             "second": time.second,
         }
-
         # eliminate empty div
         for child in article.find_all(["div"]):
             soup = BeautifulSoup(str(child), "html.parser")
             if not len(soup.text):
                 child.decompose()
-
         try:
             if article.h2 != None:
                 title = article.h2.text
@@ -67,23 +63,18 @@ def scrape():
         except:
             title = "None title"
             break
-
         try:
             if article.div != None:
-
                 # removing title article child
                 for child in article.find_all(["h2", "h1"]):
                     child.decompose()
-
                 body = article.div.text
-
             else:
                 body = article.p.text
             # body = unidecode.unidecode(body)
         except:
             # article could be without body
             body = "None body"
-
         list.append(
             {
                 "title": title,
@@ -94,3 +85,60 @@ def scrape():
             }
         )
     return list
+
+def format_message(title, body, date):
+    msg = f"{title}\n\n{body}\n\n{date}"
+    return msg
+
+def send_messages(messages):
+    file_config = open("./data/config.json")
+    config = json.load(file_config)
+    token = config["token-bot-telegram"]
+    channel_id = config["channel_id-telegram"]
+    delay = config["delay"]
+    url = (
+        config["url-telegram"]
+        .replace("{token}", token)
+        .replace("{channel_id}", channel_id)
+    )
+    for info_msg in messages:
+        message = format_message(
+            info_msg["title"], info_msg["body"], info_msg["time_string"]
+        )
+        res = requests.get(f"{url}{message}")
+        print(res)
+        time.sleep(delay)
+
+def run():
+    # init timestamp frm backup
+    obj = open("./data/last-timestamp.json")
+    config_timestamp = json.load(obj)
+    obj.close()
+    last_timestamp = config_timestamp["last-timestamp"]
+    print("---------------------------------")
+    # obtain list of scraped news with meta info
+    news = scrape()
+    print("scrape lunghezza", len(news))
+    # obtain news only after a certaint timestamp
+    news = list(filter(lambda el: el["timestamp"] > last_timestamp, news))
+    # print('filtered',len(news))
+    if len(news):
+        # news must be sent ordered by time
+        news = sorted(news, key=lambda el: el["timestamp"])
+        print("sorted", len(news))
+        # send news to group by bot
+        send_messages(news)
+        # update timestamp
+        for el in news:
+            if debug:
+                continue
+            if el["timestamp"] > last_timestamp:
+                last_timestamp = el["timestamp"]
+                # update config file for backup timestamp
+                config_timestamp["last-timestamp"] = last_timestamp
+                obj = open("./data/last-timestamp.json", "w")
+                obj.write(json.dumps(config_timestamp, indent=4))
+                obj.close()
+        print("last timestamp", last_timestamp)
+
+run()
